483751698824066d584d0bfa3f8dceb0789c4b0b
Rahul Timbaliya <rahul.timbaliya@ansibytecode.com>
2025-11-17 00:05:14 +0530

    remove Train Controller

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 require("dotenv").config({ path: ".env" });
 const router = require("./router/api");
 const routerAuth = require("./router/auth");
--- router/api.js
+++ router/api.js
@@ -8,18 +8,12 @@ const { authenticateTokenCertverse } = require("../middleware/certverseAuth");
 // Import controllers
 const feedback = require("../controller/feedbackController");
 const mailer = require("../controller/mailController");
-const trainController = require("../controller/trainController");
 const pdfController = require("../controller/examController");
 
 // Public routes
 router.post("/feedback/create", (req, res) => feedback.create(req, res));
 router.post("/mail/send", (req, res) => mailer.sendMail(req, res));
 
-router.get("/train/getTrainInfo/:number", (req, res) => trainController.getTrainInfo(req, res));
-router.get("/train/getTrainRouteInfo/:number", (req, res) => trainController.getTrainRoutInfo(req, res));
-router.post("/train/getTrainCurrentLocation", (req, res) => trainController.getTrainCurrentLocation(req, res));
-router.post("/train/getPNRInfo", (req, res) => trainController.getPNRInfo(req, res));
-router.post("/train/getBetweenTrain", (req, res) => trainController.getBetweenTrain(req, res));
 // Protected routes
 router.get("/feedback/getAll", authenticateToken, (req, res) => feedback.getAll(req, res));
 router.delete("/feedback/delete/:id", authenticateToken, (req, res) => feedback.deletedFeedback(req, res));
